b8a8eb041f52014ac0ec29ba2ba7956f4678d2d4
yTsunomoto <wasiwasimaru@gmail.com>
2019-12-21 22:18:56 +0900

    update 不足ファイルを追加。

---
+++ DotNetOwinWebApiSample.Api.Test/Shared/IntegrationTestBase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin.Testing;
+using System.Net.Http;
+
+namespace DotNetOwinWebApiSample.Api.Test.Shared
+{
+    public abstract class IntegrationTestBase
+    {
+        protected static TestServer Server { get; set; }
+        protected static HttpClient HttpClient { get; set; }
+
+        protected static void Before()
+        {
+            Server = TestServer.Create<Startup>();
+            HttpClient = Server.HttpClient;
+        }
+
+        protected static void After()
+        {
+            Server.Dispose();
+        }
+    }
+}
