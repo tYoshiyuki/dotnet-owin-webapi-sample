0758b1b5e5a445b9f3291ea947800d39cd77927b
Yoshiyuki Tsunomoto <wasiwasimaru@gmail.com>
2021-05-22 23:30:34 +0900

    update 更新。

--- GenerateOpenApi.bat
+++ GenerateOpenApi.bat
@@ -1,2 +1,5 @@
 @echo off
 .\packages\NSwag.MSBuild.13.11.1\tools\Win\nswag webapi2openapi /assembly:.\DotNetOwinWebApiSample.Api\bin\DotNetOwinWebApiSample.Api.dll /output:DotNetOwinWebApiSample.Api.json
+
+rem nswag.json を用いて出力する場合は、以下のコマンドを実施します。
+rem npx nswag run ./nswag.json /runtime:WinX64
